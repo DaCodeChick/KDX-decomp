fb1bf8a9ff4293897f57fe718a18f53830217178
DaCodeChick <peepvtuber@gmail.com>
2025-04-24 16:36:18 -0700

    Update UMemory.cpp

--- src/common/UMemory.cpp
+++ src/common/UMemory.cpp
@@ -86,7 +86,8 @@ longlong UMemory::GetAllocationCount(uint *outCount)
 
 uint UMemory::CRC(const void *inData, uint inDataSize, uint inInit)
 {
-	static uint ccitt32_crctab[256] = {
+	static uint ccitt32_crctab[256] =
+	{
 		0x00000000, 0x04C11DB7, 0x09823B6E, 0x0D4326D9,
 		0x130476DC, 0x17C56B6B, 0x1A864DB2, 0x1E475005,
 		0x2608EDB8, 0x22C9F00F, 0x2F8AD6D6, 0x2B4BCB61,
