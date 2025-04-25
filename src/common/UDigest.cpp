7419084f3ee6bb85f6d96bc833f7a66a836e542d
DaCodeChick <peepvtuber@gmail.com>
2025-04-25 13:19:25 -0700

    Update UDigest.cpp

--- src/common/UDigest.cpp
+++ src/common/UDigest.cpp
@@ -2,56 +2,56 @@
 
 #include "UMemory.h"
 
-constexpr uint F(uint x, uint y, uint z)
+constexpr uint f(uint x, uint y, uint z)
 {
     return (x & y) | (~x & z);
 }
 
-constexpr uint G(uint x, uint y, uint z)
+constexpr uint g(uint x, uint y, uint z)
 {
     return (x & z) | (y & ~z);
 }
 
-constexpr uint H(uint x, uint y, uint z)
+constexpr uint h(uint x, uint y, uint z)
 {
     return x ^ y ^ z;
 }
 
-constexpr uint I(uint x, uint y, uint z)
+constexpr uint i(uint x, uint y, uint z)
 {
     return y ^ (x | ~z);
 }
 
-constexpr uint ROTATE_LEFT(uint x, uint n)
+constexpr uint rotl(uint x, uint n)
 {
     return (x << n) | (x >> (32 - n));
 }
 
-constexpr void FF(uint &a, uint b, uint c, uint d, uint x, uint s, uint ac)
+constexpr void ff(uint &a, uint b, uint c, uint d, uint x, uint s, uint ac)
 {
-    a += F(b, c, d) + x + ac;
-    a = ROTATE_LEFT(a, s);
+    a += f(b, c, d) + x + ac;
+    a = rotl(a, s);
     a += b;
 }
 
-constexpr void GG(uint &a, uint b, uint c, uint d, uint x, uint s, uint ac)
+constexpr void gg(uint &a, uint b, uint c, uint d, uint x, uint s, uint ac)
 {
-    a += G(b, c, d) + x + ac;
-    a = ROTATE_LEFT(a, s);
+    a += g(b, c, d) + x + ac;
+    a = rotl(a, s);
     a += b;
 }
 
-constexpr void HH(uint &a, uint b, uint c, uint d, uint x, uint s, uint ac)
+constexpr void hh(uint &a, uint b, uint c, uint d, uint x, uint s, uint ac)
 {
-    a += H(b, c, d) + x + ac;
-    a = ROTATE_LEFT(a, s);
+    a += h(b, c, d) + x + ac;
+    a = rotl(a, s);
     a += b;
 }
 
-constexpr void II(uint &a, uint b, uint c, uint d, uint x, uint s, uint ac)
+constexpr void ii(uint &a, uint b, uint c, uint d, uint x, uint s, uint ac)
 {
-    a += I(b, c, d) + x + ac;
-    a = ROTATE_LEFT(a, s);
+    a += i(b, c, d) + x + ac;
+    a = rotl(a, s);
     a += b;
 }
 
@@ -125,76 +125,76 @@ void _MD5::Transform(const byte *inBlock)
     for (int i = 0; i < 16; i++)
 	{
         x[i] = *(uint *)(inBlock + (i << 2));
-        x[i] = HTONL(x[i]);
+        x[i] = htonl(x[i]);
     }
 
-    FF(a, b, c, d, x[0],  7, 0xd76aa478);
-    FF(d, a, b, c, x[1], 12, 0xe8c7b756);
-    FF(c, d, a, b, x[2], 17, 0x242070db);
-    FF(b, c, d, a, x[3], 22, 0xc1bdceee);
-    FF(a, b, c, d, x[4],  7, 0xf57c0faf);
-    FF(d, a, b, c, x[5], 12, 0x4787c62a);
-    FF(c, d, a, b, x[6], 17, 0xa8304613);
-    FF(b, c, d, a, x[7], 22, 0xfd469501);
-    FF(a, b, c, d, x[8],  7, 0x698098d8);
-    FF(d, a, b, c, x[9], 12, 0x8b44f7af);
-    FF(c, d, a, b, x[10],17, 0xffff5bb1);
-    FF(b, c, d, a, x[11],22, 0x895cd7be);
-    FF(a, b, c, d, x[12], 7, 0x6b901122);
-    FF(d, a, b, c, x[13],12, 0xfd987193);
-    FF(c, d, a, b, x[14],17, 0xa679438e);
-    FF(b, c, d, a, x[15],22, 0x49b40821);
+    ff(a, b, c, d, x[0],  7, 0xd76aa478);
+    ff(d, a, b, c, x[1], 12, 0xe8c7b756);
+    ff(c, d, a, b, x[2], 17, 0x242070db);
+    ff(b, c, d, a, x[3], 22, 0xc1bdceee);
+    ff(a, b, c, d, x[4],  7, 0xf57c0faf);
+    ff(d, a, b, c, x[5], 12, 0x4787c62a);
+    ff(c, d, a, b, x[6], 17, 0xa8304613);
+    ff(b, c, d, a, x[7], 22, 0xfd469501);
+    ff(a, b, c, d, x[8],  7, 0x698098d8);
+    ff(d, a, b, c, x[9], 12, 0x8b44f7af);
+    ff(c, d, a, b, x[10],17, 0xffff5bb1);
+    ff(b, c, d, a, x[11],22, 0x895cd7be);
+    ff(a, b, c, d, x[12], 7, 0x6b901122);
+    ff(d, a, b, c, x[13],12, 0xfd987193);
+    ff(c, d, a, b, x[14],17, 0xa679438e);
+    ff(b, c, d, a, x[15],22, 0x49b40821);
 
-    GG(a, b, c, d, x[1],  5, 0xf61e2562);
-    GG(d, a, b, c, x[6],  9, 0xc040b340);
-    GG(c, d, a, b, x[11],14, 0x265e5a51);
-    GG(b, c, d, a, x[0], 20, 0xe9b6c7aa);
-    GG(a, b, c, d, x[5],  5, 0xd62f105d);
-    GG(d, a, b, c, x[10], 9, 0x02441453);
-    GG(c, d, a, b, x[15],14, 0xd8a1e681);
-    GG(b, c, d, a, x[4], 20, 0xe7d3fbc8);
-    GG(a, b, c, d, x[9],  5, 0x21e1cde6);
-    GG(d, a, b, c, x[14], 9, 0xc33707d6);
-    GG(c, d, a, b, x[3], 14, 0xf4d50d87);
-    GG(b, c, d, a, x[8], 20, 0x455a14ed);
-    GG(a, b, c, d, x[13], 5, 0xa9e3e905);
-    GG(d, a, b, c, x[2],  9, 0xfcefa3f8);
-    GG(c, d, a, b, x[7], 14, 0x676f02d9);
-    GG(b, c, d, a, x[12],20, 0x8d2a4c8a);
+    gg(a, b, c, d, x[1],  5, 0xf61e2562);
+    gg(d, a, b, c, x[6],  9, 0xc040b340);
+    gg(c, d, a, b, x[11],14, 0x265e5a51);
+    gg(b, c, d, a, x[0], 20, 0xe9b6c7aa);
+    gg(a, b, c, d, x[5],  5, 0xd62f105d);
+    gg(d, a, b, c, x[10], 9, 0x02441453);
+    gg(c, d, a, b, x[15],14, 0xd8a1e681);
+    gg(b, c, d, a, x[4], 20, 0xe7d3fbc8);
+    gg(a, b, c, d, x[9],  5, 0x21e1cde6);
+    gg(d, a, b, c, x[14], 9, 0xc33707d6);
+    gg(c, d, a, b, x[3], 14, 0xf4d50d87);
+    gg(b, c, d, a, x[8], 20, 0x455a14ed);
+    gg(a, b, c, d, x[13], 5, 0xa9e3e905);
+    gg(d, a, b, c, x[2],  9, 0xfcefa3f8);
+    gg(c, d, a, b, x[7], 14, 0x676f02d9);
+    gg(b, c, d, a, x[12],20, 0x8d2a4c8a);
 
-    HH(a, b, c, d, x[5],  4, 0xfffa3942);
-    HH(d, a, b, c, x[8], 11, 0x8771f681);
-    HH(c, d, a, b, x[11],16, 0x6d9d6122);
-    HH(b, c, d, a, x[14],23, 0xfde5380c);
-    HH(a, b, c, d, x[1],  4, 0xa4beea44);
-    HH(d, a, b, c, x[4], 11, 0x4bdecfa9);
-    HH(c, d, a, b, x[7], 16, 0xf6bb4b60);
-    HH(b, c, d, a, x[10],23, 0xbebfbc70);
-    HH(a, b, c, d, x[13], 4, 0x289b7ec6);
-    HH(d, a, b, c, x[0], 11, 0xeaa127fa);
-    HH(c, d, a, b, x[3], 16, 0xd4ef3085);
-    HH(b, c, d, a, x[6], 23, 0x04881d05);
-    HH(a, b, c, d, x[9],  4, 0xd9d4d039);
-    HH(d, a, b, c, x[12],11, 0xe6db99e5);
-    HH(c, d, a, b, x[15],16, 0x1fa27cf8);
-    HH(b, c, d, a, x[2], 23, 0xc4ac5665);
+    hh(a, b, c, d, x[5],  4, 0xfffa3942);
+    hh(d, a, b, c, x[8], 11, 0x8771f681);
+    hh(c, d, a, b, x[11],16, 0x6d9d6122);
+    hh(b, c, d, a, x[14],23, 0xfde5380c);
+    hh(a, b, c, d, x[1],  4, 0xa4beea44);
+    hh(d, a, b, c, x[4], 11, 0x4bdecfa9);
+    hh(c, d, a, b, x[7], 16, 0xf6bb4b60);
+    hh(b, c, d, a, x[10],23, 0xbebfbc70);
+    hh(a, b, c, d, x[13], 4, 0x289b7ec6);
+    hh(d, a, b, c, x[0], 11, 0xeaa127fa);
+    hh(c, d, a, b, x[3], 16, 0xd4ef3085);
+    hh(b, c, d, a, x[6], 23, 0x04881d05);
+    hh(a, b, c, d, x[9],  4, 0xd9d4d039);
+    hh(d, a, b, c, x[12],11, 0xe6db99e5);
+    hh(c, d, a, b, x[15],16, 0x1fa27cf8);
+    hh(b, c, d, a, x[2], 23, 0xc4ac5665);
 
-    II(a, b, c, d, x[0],  6, 0xf4292244);
-    II(d, a, b, c, x[7], 10, 0x432aff97);
-    II(c, d, a, b, x[14],15, 0xab9423a7);
-    II(b, c, d, a, x[5], 21, 0xfc93a039);
-    II(a, b, c, d, x[12], 6, 0x655b59c3);
-    II(d, a, b, c, x[3], 10, 0x8f0ccc92);
-    II(c, d, a, b, x[10],15, 0xffeff47d);
-    II(b, c, d, a, x[1], 21, 0x85845dd1);
-    II(a, b, c, d, x[8],  6, 0x6fa87e4f);
-    II(d, a, b, c, x[15],10, 0xfe2ce6e0);
-    II(c, d, a, b, x[6], 15, 0xa3014314);
-    II(b, c, d, a, x[13],21, 0x4e0811a1);
-    II(a, b, c, d, x[4],  6, 0xf7537e82);
-    II(d, a, b, c, x[11],10, 0xbd3af235);
-    II(c, d, a, b, x[2], 15, 0x2ad7d2bb);
-    II(b, c, d, a, x[9], 21, 0xeb86d391);
+    ii(a, b, c, d, x[0],  6, 0xf4292244);
+    ii(d, a, b, c, x[7], 10, 0x432aff97);
+    ii(c, d, a, b, x[14],15, 0xab9423a7);
+    ii(b, c, d, a, x[5], 21, 0xfc93a039);
+    ii(a, b, c, d, x[12], 6, 0x655b59c3);
+    ii(d, a, b, c, x[3], 10, 0x8f0ccc92);
+    ii(c, d, a, b, x[10],15, 0xffeff47d);
+    ii(b, c, d, a, x[1], 21, 0x85845dd1);
+    ii(a, b, c, d, x[8],  6, 0x6fa87e4f);
+    ii(d, a, b, c, x[15],10, 0xfe2ce6e0);
+    ii(c, d, a, b, x[6], 15, 0xa3014314);
+    ii(b, c, d, a, x[13],21, 0x4e0811a1);
+    ii(a, b, c, d, x[4],  6, 0xf7537e82);
+    ii(d, a, b, c, x[11],10, 0xbd3af235);
+    ii(c, d, a, b, x[2], 15, 0x2ad7d2bb);
+    ii(b, c, d, a, x[9], 21, 0xeb86d391);
 
     mState[0] += a;
     mState[1] += b;
@@ -228,7 +228,7 @@ void _MD5::Report(void *outDigest)
     Transform(mBuffer);
 
     for (int i = 0; i < 4; ++i)
-        ((uint *)outDigest)[i] = HTONL(mState[i]);
+        ((uint *)outDigest)[i] = htonl(mState[i]);
 
     Clear(0x5c);
 }
