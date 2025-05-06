ed9c69ea1a0bb4c22ed4bf0d016e439a7bba7ee0
DaCodeChick <peepvtuber@gmail.com>
2025-05-06 14:21:25 -0700

    Pascal string routines

--- src/common/ANSI.h
+++ src/common/ANSI.h
@@ -12,8 +12,12 @@ extern "C"
 	unsigned pstrcat(unsigned char *dst, const unsigned char *src);
 	unsigned pstrncat(unsigned char *dst, const unsigned char *src, unsigned len);
 	bool pstrcmp(const unsigned char *str1, const unsigned char *str2);
+	bool pstrncmp(const unsigned char *str1, const unsigned char *str2, unsigned len);
 	unsigned char *pstrchr(const unsigned char *str, unsigned char chr);
 	unsigned char *pstrrchr(const unsigned char *str, unsigned char chr);
+	void pstrrev(unsigned char *str);
+	unsigned char *pstrstr(const unsigned char *str1, unsigned len1, const unsigned char *str2,
+	                       unsigned len2);
 
 #ifdef __cplusplus
 }
